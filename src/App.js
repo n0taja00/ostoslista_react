265f4964caff08f17ae9ef470b35b3e0674d399e
Jaako Taskila <n0taja00@students.oamk.fi>
2021-11-08 19:10:16 +0200

    valmis työ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from "react";
+import axios from  "axios";
+
+const URL = "http://localhost/ostoslista/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [item, setItem] = useState([]);
+    const [text,setText] = useState("");
+    const [amount,setAmount] = useState("");
+
+    function save(e) {
+      e.preventDefault();
+      const json = JSON.stringify({description:text, amount:amount})
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) =>{
+      setItem(item => [...item,response.data]);
+      setText('');
+      setAmount('');
+    }).catch (error => {
+      alert(error.response.data.error)
+    });
+    }
+
+    function remove(id) {
+      const json = JSON.stringify({id:id})
+      axios.post(URL + 'delete.php', json, {
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+      .then((response) => {
+        const newListWithoutRemoved = item.filter((item) => item.id !== id);
+        setItem(newListWithoutRemoved);
+      }).catch (error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+      }
+
+    useEffect(()=> {
+      axios.get(URL)
+      .then((response)=> {
+        console.log(response.data);
+        setItem(response.data);
+      }).catch(error => {
+        alert(error);
+      })
+    },[])
+
+return (
+  <form onSubmit={save}>
+     <h3>Shopping list</h3>
+ <div>
+   <label>New item</label>
+   <input type="text"
+   onChange={e => setText(e.target.value)}
+   value={text} placeholder="type description" />
+ </div>
+ <div id="amount">
+  <input type="number" step="1"
+  onChange={e => setAmount(e.target.value)}
+  value={amount} placeholder="type amount"/>
+ </div>
+ <div id="button">
+ <button>Add</button>
+ </div>
+ <output>
+   <ol>
+    {item?.map(text => (
+      <li key={text.id}>{text.description}, {text.amount} &nbsp;
+      <a href="#" classname="delete" onClick={() => remove(text.id)}>Delete</a>
+      </li>
+    ))}
+  </ol>
+  </output>
+   </form>
+)
+
 }
 
 export default App;
